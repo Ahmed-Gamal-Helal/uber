73d3bef3fe46fee8276058ab79e342d9d130a75d
Jemy1234 <ahmdtawfiek@gmail.com>
2018-01-15 09:46:31 +0200

    uber

--- js/main.js
+++ js/main.js
@@ -1,9 +1,35 @@
 $(function(){
     'use strict';
-    $("nav-tabs ul li a, ul.navbar-nav li").click(function (){
-        $(this).addClass('active').siblings().removeClass('active');
+    // $("nav-tabs ul li a, ul.navbar-nav li").click(function (){
+        // $(this).addClass('active').siblings().removeClass('active');
         // $(this).attr('color', '#F00 !important');
-    });
+    // });
 
 });
 
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover({
+      html:true
+    });   
+        $('[rel="popover"]').popover({
+        container: 'body',
+        html: true,
+        content: function () {
+            var clone = $($(this).data('popover-content')).clone(true).removeClass('hide');
+            return clone;
+        }
+    }).click(function(e) {
+        e.preventDefault();
+    });
+});
+
+// Google MAps
+function myMap() {
+    var mapProp= {
+        center:new google.maps.LatLng(30.0444,31.2357),
+        zoom:5,
+    };
+    var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    }
+
+// Animation
